a8a330b887e28bb8003c46eb122cf9a3dbfcbc62
Randel Eng <bobaloeng@gmail.com>
2016-02-19 23:30:41 -0700

    First upload of assignment 3

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,73 @@
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Initializes the total to 0 for the calculator.
+	 */
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Gets the total for the calculations
+	 * 
+	 * @return total of the calculations
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds the number to the total
+	 * 
+	 * @param value the number you want to add
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the number from the total
+	 * 
+	 * @param value the number you want to subtract
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies the number to the total
+	 * 
+	 * @param value the number you want to multiply
+	 */
+	
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides the number from the total
+	 * 
+	 * @param value the number you want to divide with
+	 */
+	
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Prints out the history of the calculator
+	 * 
+	 * @return a string of the history
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
