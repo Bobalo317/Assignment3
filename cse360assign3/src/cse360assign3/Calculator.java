9014049f107880ca3dbd078e936b1037181e9376
Randel Eng <bobaloeng@gmail.com>
2016-02-20 00:02:27 -0700

    Second upload, added the add, subtract, getTotal, multiply, divide functions and junit test for them

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -19,7 +19,7 @@ public class Calculator {
 	 */
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
@@ -29,7 +29,7 @@ public class Calculator {
 	 */
 	
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	/**
@@ -39,7 +39,7 @@ public class Calculator {
 	 */
 	
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	/**
@@ -49,7 +49,7 @@ public class Calculator {
 	 */
 	
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	/**
@@ -59,7 +59,12 @@ public class Calculator {
 	 */
 	
 	public void divide (int value) {
-		
+		if (value != 0){
+			total = total / value;
+		}
+		else{
+			total = 0;
+		}
 	}
 	
 	/**
--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -9,32 +9,45 @@ public class CalculatorTest {
 	@Test
 	public void testCalculator() {
 		Calculator cal = new Calculator();
-		assertNotNull(cal);
+		assertNotNull(cal); //checks if the calculator constructor works, to see if its not null
 	}
 	
 	@Test
 	public void testGetTotal() {
-		fail("Not yet implemented");
+		Calculator cal = new Calculator();
+		assertEquals(0, cal.getTotal()); //Didn't change total so it should default to zero, and we are checking if it equals 0
 	}
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		Calculator cal = new Calculator();
+		cal.add(5); // adds 5 to the total so it should be just 5
+		assertEquals(5, cal.getTotal()); //checks if 5 is equal to the total
 	}
 	
 	@Test
 	public void testSubtract() {
-		fail("Not yet implemented");
+		Calculator cal = new Calculator();
+		cal.subtract(5); //subtracts 5 to the total which is zero
+		assertEquals(-5, cal.getTotal());
 	}
 	
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		Calculator cal = new Calculator();
+		cal.add(2); //add something to total so it would not just be 0
+		cal.multiply(4); //multiply the total by 4 to see if it works
+		assertEquals(8, cal.getTotal()); //check if the multiply function works with predetermined value
 	}
 	
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		Calculator cal = new Calculator();
+		cal.divide(0); //test that dividing by 0 returns a 0
+		assertEquals(0, cal.getTotal());
+		cal.add(4); // add 4 to total to check divide function
+		cal.divide(2); //divide by 2 to see if it works
+		assertEquals(2, cal.getTotal()); // checks if the divide function works with predetermined value
 	}
 	
 	@Test
